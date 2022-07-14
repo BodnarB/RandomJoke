35e0964bd5cf7d700060935700f1c8478a617bdc
Boglarka Bodnar <bodnarbog94@gmail.com>
2022-07-14 18:10:29 +0200

    up

---
+++ joke.js
@@ -0,0 +1,27 @@
+let theJoke = document.querySelector('.js-btn');
+let jokePlace = document.querySelector('.js-thejoke');
+
+function getAjoke(event) {
+    let apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,racist';
+    fetch(apiUrl).then(convertToJson).then(displayJoke);
+}
+
+function convertToJson(data) {
+    return data.json();
+}
+
+function displayJoke(randomjoke) {
+    // console.log(randomjoke)
+    let htmlJoke = `<div class="joke-box"><p>${randomjoke.joke}</p></div>`;
+    let htmlLongJoke = `<div class="joke-box"><p>${randomjoke.setup}</p><p>${randomjoke.delivery}</p></div>`;
+    if (randomjoke.joke == undefined)
+        jokePlace.innerHTML = htmlLongJoke;
+    if (randomjoke.setup == undefined)
+        jokePlace.innerHTML = htmlJoke;
+    theJoke.innerHTML = 'Get another joke'
+}
+
+
+
+
+theJoke.addEventListener('click', getAjoke)
